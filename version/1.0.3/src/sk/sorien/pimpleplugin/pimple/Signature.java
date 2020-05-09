6f144200e70edcec2371aad20a197e4530dc2a66
mmkulikov <mmkulikov@yandex.ru>
2020-05-09 12:54:29 +0200

    2020

---
+++ version/1.0.3/src/sk/sorien/pimpleplugin/pimple/Signature.java
@@ -0,0 +1,77 @@
+package sk.sorien.pimpleplugin.pimple;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Stanislav Turza
+ */
+public class Signature {
+
+    public String base = "";
+    public String parameter = "";
+
+    public Signature(@Nullable String expression) {
+        set(expression);
+    }
+
+    public Signature() {
+    }
+
+    public void set(@Nullable String expression) {
+        base = "";
+        parameter = "";
+
+        try
+        {
+            if (expression == null) {
+                return;
+            }
+
+            int len = expression.length();
+            if (len > 0) {
+                int start = -1;
+                int counter = 0;
+
+                if (expression.charAt(len - 1) == ']') {
+                    for (int i = (len - 2); i >= 0; i--){
+                        if ((expression.charAt(i) == '[') && (counter == 0)) {
+                            start = i;
+                            break;
+                        }
+
+                        if ((expression.charAt(i) == ']') ) {
+                            counter++;
+                        }
+
+                        if ((expression.charAt(i) == '[') ) {
+                            counter--;
+                        }
+                    }
+                }
+
+                if (start == -1) {
+                    base = expression;
+                    return;
+                }
+
+                base = expression.substring(0, start);
+                parameter = expression.substring(start + 1, len - 1);
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(expression);
+        }
+    }
+
+    public Boolean hasParameter() {
+        return !parameter.isEmpty();
+    }
+
+    public Boolean hasValidClassSignature() {
+        return !(base.isEmpty() || base.matches("#C\\\\(array|int|integer|float|bool|boolean|string)"));
+    }
+
+    @Override
+    public String toString() {
+        return base + (this.hasParameter() ? "[" + parameter + "]" : "");
+    }
+}
